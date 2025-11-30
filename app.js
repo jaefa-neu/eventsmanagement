844877cef193e41999696bcbceb9cb5a78513e9a
jaefa-neu <jeaangelline.efa@neu.edu.ph>
2025-11-30 13:36:30 +0800

    Final final na promise

--- app.js
+++ app.js
@@ -33,8 +33,7 @@ const options = {
     },
     servers: [
       {
-        url: "http://localhost:3000",
-        description: "Local Server",
+        url: "http://localhost:3000"
       },
     ],
   },
